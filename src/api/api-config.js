0704d6e0cdeea4e5fcfbd0ffa55047ff3dbaa172
johnlemon93 <quangchanh.11tclc.dut@gmail.com>
2019-03-02 22:36:33 +0700

    resolved #3

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,2 +1,4 @@
-export const API_ROOT = "https://ao-thun-vnxk.herokuapp.com/api";
+import config from '../../config.json';
+
+export const API_ROOT = config.API_ROOT;
 export const STATIC_SERVER_ROOT = "";
